8e5201cb2ab2b90f474f3526d4bc02c22ae47325
Marcel <236373202+marcel-erbas@users.noreply.github.com>
2025-10-17 23:43:10 +0200

    create map element for each animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,6 +8,7 @@ def load_data(file_path):
 
 
 animals_data = load_data('animals_data.json')
+output = ""
 
 for fox in animals_data:
     fox_name = fox.get("name")
@@ -15,12 +16,25 @@ for fox in animals_data:
     fox_locations = ", ".join(list(fox.get("locations")))
     fox_type = fox.get("characteristics").get("type")
 
+    output += "<li class=\"cards__item\">"
     if fox_name:
-        print(f"Name: {fox_name}")
+        output += f"Name: {fox_name}<br/>\n"
     if fox_diet:
-        print(f"Diet: {fox_diet}")
+        output += f"Diet: {fox_diet}<br/>\n"
     if fox_locations:
-        print(f"Location: {fox_locations}")
+        output += f"Location: {fox_locations}<br/>\n"
     if fox_type:
-        print(f"Type: {fox_type}")
-    print()
+        output += f"Type: {fox_type}<br/>\n"
+    output += '</li>'
+    output += "\n"
+
+print(output)
+
+with open("animals_template.html", "r") as f:
+    animals_html_text = f.read()
+
+updated_html = animals_html_text.replace("__REPLACE_ANIMALS_INFO__", output)
+
+with open("animals.html", "w") as f:
+    f.write(updated_html)
+
